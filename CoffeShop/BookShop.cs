74c27552c1db0bee42e16ae8e55c45425ed04501
Afsarud <engafsar5577@gmail.com>
2019-09-13 02:04:43 +0600

    udpate

---
+++ CoffeShop/BookShop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeShop
+{
+    public partial class BookShop : Form
+    {
+        public BookShop()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            resultRichTextBox.Text = "Customer Name: "+textBox1.Text + ", " +
+                " " + "Contact Number: "+textBox2.Text + ", " +
+                " " +"Your Address"+textBox3.Text+ "Order Amount: "+ orderComboBox.SelectedItem + ", " +
+                " " + "Quantity Number: "+textBox4.Text+", " +
+                " "+"Total Price"+Convert.ToInt32(orderComboBox.Text)* Convert.ToInt32(textBox4.Text);
+        }
+    }
+}
---
+++ CoffeShop/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeShop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public object ComboBox1 { get; private set; }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            richTextBox1.Text = "Customer Name: "+ CusNameTextBox.Text + "," +
+                " "+ "Contact Number: " + contactTextBox.Text+ "," +
+                " " + "Address: " + addressTextBox.Text+ ", " +
+                " " +"Order Amount:" + orderComboBox.SelectedItem + " ," +
+                " " +"Quantity Value: " + quantityTextBox.Text+ " ," +
+                " " + "Total Price: "+Convert.ToInt32( orderComboBox.SelectedItem)* Convert.ToInt32(quantityTextBox.Text);
+        }
+    }
+}
